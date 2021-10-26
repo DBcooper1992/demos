c66dcffb329d8f2a792c616ae1cd8cb66685045e
nick <772844248@qq.com>
2021-10-26 20:17:27 +0800

    添加四种配置文件，以及简单的自定义配置@Value

--- src/main/java/com/nicksui/wiki/controller/TestController.java
+++ src/main/java/com/nicksui/wiki/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.nicksui.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.tools.java.Environment;
 
 //返回字符串
 @RestController
@@ -12,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 //@Controller
 public class TestController {
 
+    @Value("${test.hello:TEST}")//冒号后是默认配置，当无配置时候，采用冒号后的默认值。
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world!"+testHello;
     }
 
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "Hello World! Post, "+name;
+
     }
 }
